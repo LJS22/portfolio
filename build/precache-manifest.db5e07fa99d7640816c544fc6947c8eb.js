37be8fd4279d4dce9e0cfa422c4da0af378ed6c9
LJS22 <LJS22@users.noreply.github.com>
2020-09-15 09:44:25 +0100

    final change

---
+++ build/precache-manifest.db5e07fa99d7640816c544fc6947c8eb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c37dd3d9e5baacfe4541a93ca94b47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9d3d3ac63362d09debb2",
+    "url": "/portfolio/static/css/main.8d589c7b.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3d3ac63362d09debb2",
+    "url": "/portfolio/static/js/main.b045cb45.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a0e3593238694db45b80ec2a4789f66d",
+    "url": "/portfolio/static/media/Arrow.a0e35932.png"
+  },
+  {
+    "revision": "ff7d8d97e14464af27169aacf237cbc1",
+    "url": "/portfolio/static/media/BMI.ff7d8d97.png"
+  },
+  {
+    "revision": "cf627ced09590d77d4a59d3a5db2707a",
+    "url": "/portfolio/static/media/Calculator.cf627ced.jpg"
+  },
+  {
+    "revision": "b0f64c72335ac13a78d9ab273e115f69",
+    "url": "/portfolio/static/media/ContactsIcon.b0f64c72.jpg"
+  },
+  {
+    "revision": "20e7c654b449105f3fcc1110a0289366",
+    "url": "/portfolio/static/media/Dice.20e7c654.png"
+  },
+  {
+    "revision": "03c41356a11e144e924643cee2ef4465",
+    "url": "/portfolio/static/media/Drum.03c41356.png"
+  },
+  {
+    "revision": "a7e1275b9bf2ad2abf8a12fc193f7f1d",
+    "url": "/portfolio/static/media/EmailIcon.a7e1275b.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/portfolio/static/media/Github.3e54ed15.png"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "/portfolio/static/media/JSImage.94ae2dcd.png"
+  },
+  {
+    "revision": "cf84af19a17162178a96190b8f593be0",
+    "url": "/portfolio/static/media/MeNoBG.cf84af19.png"
+  },
+  {
+    "revision": "b10b140c6a5e314a0eccf161c5d4f112",
+    "url": "/portfolio/static/media/MongoDB.b10b140c.png"
+  },
+  {
+    "revision": "67bce85dfa17ca93f66e1927d98444ae",
+    "url": "/portfolio/static/media/MyLogo.67bce85d.jpg"
+  },
+  {
+    "revision": "854ac896af0919189fbac8eedd3ec98a",
+    "url": "/portfolio/static/media/NodeJS.854ac896.png"
+  },
+  {
+    "revision": "2a8b77d3212d5813a9a1b2fac05892f3",
+    "url": "/portfolio/static/media/ProjectsIcon.2a8b77d3.jpg"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/portfolio/static/media/ReactLogo.c3ba82a5.png"
+  },
+  {
+    "revision": "f8eda46a4c33efcd673024f15ca5858a",
+    "url": "/portfolio/static/media/Surman.f8eda46a.png"
+  },
+  {
+    "revision": "080d0fda628d14d4d3f14992c44dad02",
+    "url": "/portfolio/static/media/ToDo.080d0fda.png"
+  },
+  {
+    "revision": "c3ce0c15c4fa916c837e84474bf143d1",
+    "url": "/portfolio/static/media/cssImage.c3ce0c15.png"
+  },
+  {
+    "revision": "0ea316dbe0fd5dab21e160b3f48fa6b5",
+    "url": "/portfolio/static/media/html.0ea316db.png"
+  }
+]);
